f4732caef3540d855fcd986c2baf637c0c32a7a4
kressnik <bevz587@gmail.com>
2021-05-03 21:44:40 +0300

    create(db): added database connect

--- index.js
+++ index.js
@@ -3,17 +3,32 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const sequelize = require('./db/index.js');
 const serverRoutes = require('./routers/index.js');
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(serverRoutes);
-app.use(defaultRoute);
-app.listen(PORT, () => {
-  console.log(`Server: Start OK. Port ${PORT}`);
-});
+async function start() {
+  try {
+
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    const app = express();
+
+    app.use(express.static(path.resolve(__dirname, 'static')));
+    app.use(serverRoutes);
+    app.use(defaultRoute);
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+start();
 
 function defaultRoute(req, res) {
   res.status(404).send({
@@ -24,4 +39,4 @@ function defaultRoute(req, res) {
     meassgeCode: 'SYSTEM_ROUTE_NOT_FOUND',
     method: req.method,
   });
-};
+}
