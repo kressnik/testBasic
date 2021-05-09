19eee8e0536b18b9f11b2149c38226b2e766c908
kressnik <bevz587@gmail.com>
2021-05-10 00:35:37 +0300

    feat(calculations): Added interfaces for calculations

--- server/controllers/calculate.js
+++ server/controllers/calculate.js
@@ -3,16 +3,49 @@
 const { validationResult } = require('express-validator');
 
 const numberHandler = (req, res) => {
-  console.log(req.params, req.body);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
   res.status(200).json({
-    status: 'OK',
-    data: [1, 2, 3, 4, 5, 6, 7]
+    code: 200,
+    data: {
+      number: 5,
+      median: 10,
+      arithmeticMean: 5
+    },
+    message: 'Calculation data successfully',
+    messageCode: 'CALCULATION_DATA_OK',
+    time: Date.now()
+  });
+};
+
+const getHistoryHandler = (req, res) => {
+  res.status(200).json({
+    code: 200,
+    data: [
+      {
+        number: 5,
+        median: 10,
+        arithmeticMean: 5
+      }
+    ],
+    message: 'Calculation history received successfully',
+    messageCode: 'HISTORY_GET_OK',
+    time: Date.now()
+  });
+};
+
+const deleteCalculateHandler = (req, res) => {
+  res.status(200).json({
+    code: 200,
+    message: 'Removed calculation',
+    messageCode: 'DELETE_OK',
+    time: Date.now()
   });
 };
 
 exports.numberHandler = numberHandler;
+exports.getHistoryHandler = getHistoryHandler;
+exports.deleteCalculateHandler = deleteCalculateHandler;
--- server/routers/calculate.js
+++ server/routers/calculate.js
@@ -1,13 +1,23 @@
 'use strict';
 const express = require('express');
 const { body } = require('express-validator');
-const { numberHandler } = require('../controllers/calculate.js');
+const { numberHandler, getHistoryHandler, deleteCalculateHandler } = require('../controllers/calculate.js');
 const router = express.Router();
 
 router.post(
   '/api/calculate',
   body('number').trim().isNumeric().toInt(),
   numberHandler
+);
+
+router.get(
+  '/api/calculationHistory',
+  getHistoryHandler
+);
+
+router.delete(
+  '/api/calculate/:id',
+  deleteCalculateHandler
 );
 
 module.exports = router;
